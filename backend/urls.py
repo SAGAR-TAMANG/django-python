23fd41b43286cef3795c3313756d43cc7c40f62f
Sagar Tamang <sagar.bdr0000@gmail.com>
2023-07-08 09:47:05 +0530

    added static template

--- backend/urls.py
+++ backend/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('playground/', include('playground.urls'))
+    path('playground/', include('playground.urls')),
+    path('fitapp/', include('fitapp.urls')),
 ]
